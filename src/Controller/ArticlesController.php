2f279c34285687def6b3407e943f599cd0d8503b
awmrozek <mrozekss@gmail.com>
2022-08-27 21:04:40 +0200

    Added login page for app using framework

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -5,106 +5,99 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
-	public function initialize(): void
-	{
-		parent::initialize();
+    public function initialize(): void
+    {
+        parent::initialize();
 
-		$this->loadComponent('Paginator');
-		$this->loadComponent('Flash'); // Include the FlashComponent
-	}
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash'); // Include the FlashComponent
+    }
 
-	public function index()
-	{
-		$articles = $this->Paginator->paginate($this->Articles->find());
-		$this->set(compact('articles'));
-	}
+    public function index()
+    {
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
 
-	public function view($slug)
-	{
-		$article = $this->Articles
-			->findBySlug($slug)
-			->contain('Tags')
-			->firstOrFail();
-		$this->set(compact('article'));
-	}
+    public function view($slug)
+    {
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->contain('Tags')
+            ->firstOrFail();
+        $this->set(compact('article'));
+    }
 
-	public function add()
-	{
-		$article = $this->Articles->newEmptyEntity();
-		if ($this->request->is('post')) {
-			$article = $this->Articles->patchEntity($article, $this->request->getData());
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
 
-			// Hardcoding the user_id is temporary, and will be removed later
-			// when we build authentication out.
-			$article->user_id = 1;
+            // Hardcoding the user_id is temporary, and will be removed later
+            // when we build authentication out.
+            $article->user_id = 1;
 
-			if ($this->Articles->save($article)) {
-				$this->Flash->success(__('Your article has been saved.'));
-				return $this->redirect(['action' => 'index']);
-			}
-			$this->Flash->error(__('Unable to add your article.'));
-		}
-		// Get a list of tags.
-		$tags = $this->Articles->Tags->find('list')->all();
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        // Get a list of tags.
+        $tags = $this->Articles->Tags->find('list')->all();
 
-		// Set tags to the view context
-		$this->set('tags', $tags);
+        // Set tags to the view context
+        $this->set('tags', $tags);
 
-		$this->set('article', $article);
-	}
+        $this->set('article', $article);
+    }
 
+    public function edit($slug)
+    {
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->contain('Tags')
+            ->firstOrFail();
 
-	public function edit($slug)
-	{
-		$article = $this->Articles
-			->findBySlug($slug)
-			->contain('Tags')
-			->firstOrFail();
+        if ($this->request->is(['post', 'put'])) {
+            $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been updated.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
 
-		if ($this->request->is(['post', 'put'])) {
-			$this->Articles->patchEntity($article, $this->request->getData());
-			if ($this->Articles->save($article)) {
-				$this->Flash->success(__('Your article has been updated.'));
-				return $this->redirect(['action' => 'index']);
-			}
-			$this->Flash->error(__('Unable to update your article.'));
-		}
+        $tags = $this->Articles->Tags->find('list')->all();
+        $this->set('tags', $tags);
+        $this->set('article', $article);
+    }
 
-		$tags = $this->Articles->Tags->find('list')->all();
-		$this->set('tags', $tags);
-		$this->set('article', $article);
-	}
+    public function delete($slug)
+    {
+        $this->request->allowMethod(['post', 'delete']);
 
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        if ($this->Articles->delete($article)) {
+            $this->Flash->success(__('The {0} article has been deleted.', $article->title));
+            return $this->redirect(['action' => 'index']);
+        }
+    }
 
-	public function delete($slug)
-	{
-		$this->request->allowMethod(['post', 'delete']);
+    public function tags(...$tags)
+    {
+        // Use the ArticlesTable to find tagged articles.
+        $articles = $this->Articles->find('tagged', [
+                'tags' => $tags
+        ])
+            ->all();
 
-		$article = $this->Articles->findBySlug($slug)->firstOrFail();
-		if ($this->Articles->delete($article)) {
-			$this->Flash->success(__('The {0} article has been deleted.', $article->title));
-			return $this->redirect(['action' => 'index']);
-		}
-	}
-
-
-
-	public function tags(...$tags)
-	{
-		// Use the ArticlesTable to find tagged articles.
-		$articles = $this->Articles->find('tagged', [
-				'tags' => $tags
-		])
-			->all();
-
-		// Pass variables into the view template context.
-		$this->set([
-				'articles' => $articles,
-				'tags' => $tags
-		]);
-	}
-
-
+        // Pass variables into the view template context.
+        $this->set([
+                'articles' => $articles,
+                'tags' => $tags
+        ]);
+    }
 }
-
 
